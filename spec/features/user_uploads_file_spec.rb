60d18cb124c40a7ac66e315088313822d4ba94c5
Olivia <oereeve@gmail.com>
2016-01-21 12:41:12 -0500

    small changes

--- spec/features/user_uploads_file_spec.rb
+++ spec/features/user_uploads_file_spec.rb
@@ -9,10 +9,11 @@ feature "assignment" do
     visit assignments_path
     click_link "Add New Assignment"
 
-    fill_in "Title", with: "Noodles!"
+    fill_in "Title", with: "This is a sample PDF!"
     attach_file "File", "#{Rails.root}/spec/support/PDFs/sample1.pdf"
     click_button "Add Assignment"
 
     expect(page).to have_content("Assignment added successfully")
+    expect(page).to have_content("This is a sample PDF!")
   end
 end
